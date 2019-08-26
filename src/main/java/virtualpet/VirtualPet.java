9e0e25c93518ec7fbfa56bc9067c94eca39849bc
Caleb <sharpcaleb007@gmail.com>
2019-08-25 21:27:02 -0400

    v pet introduction

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -16,6 +16,26 @@ public class VirtualPet {
 		this.needMail = false;
 	}
 	
+	public void status() {
+		System.out.println("              " + name);
+		System.out.println("________________________________");
+		System.out.println();
+		System.out.println("Hunger level:		|  " + hunger);
+		System.out.println("Thirst level:		|  " + thirst);
+		System.out.println("Boredom is:		|  " + boredom);
+		System.out.println("Mail needs checked:	|  " + needMail);
+	}
+	
+	public void selection() {
+		System.out.println("Make a selection: ");
+		System.out.println("1. Feed " + name);
+		System.out.println("2. Water " + name);
+		System.out.println("3. Play fetch with " + name);
+		System.out.println("4. Tell " + name + " to roll over");
+		System.out.println("5. Ask " + name + " to shake");
+		System.out.println("6. Have " + name + " check the mail");
+	}
+	
 	public void feed() {
 		System.out.println("You feed " + name + ".");
 		hunger -= 5;
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -10,24 +10,51 @@ public class VirtualPetApp {
 		String petName = introduction();
 		VirtualPet pet = new VirtualPet(petName);
 		
-		
+		pet.status();
+		System.out.println();
+		promptEnterKey();
+		pet.selection();
 
 	}
 
 	public static String introduction() {
 		System.out.println("Hello, welcome to your virtual pet game!");
-
-		System.out.println("Please choose the type of pet you want: ");
+//		System.out.println();
+		promptEnterKey();
+		System.out.println("In this game you will decide what type of pet you want and choose a name for it. \n"
+				+ "You will also be able to make choices for what to do with your pet, based on how it is feeling. \n"
+				+ "Whenever you want to check the status of your pet, just enter '0'.");
+		System.out.println();
+		System.out.println("This is so exciting! Let's get started.");
+		promptEnterKey();
+		System.out.print("Please enter the type of pet you want: ");
 		String petType;
 		petType = input.nextLine();
+		System.out.println();
 		System.out.println("Great! Now, lets give your " + petType + " a name.");
-
-		System.out.println("Enter your " + petType + "'s name: ");
+		promptEnterKey();
+		System.out.print("Enter your " + petType + "'s name: ");
 		String petName;
 		petName = input.nextLine();
-
-		System.out.println("This is so exciting! Let's get " + petName + " all set up.");
+		System.out.println();
+		System.out.println("WOW! What an awesome name!!");
+		promptEnterKey();
+		System.out.println();
+		System.out.println("We hope you enjoy taking care of " + petName + ". Next, you will be given an option menu where you" +
+		" can make selections for what \n"
+				+ "you would like to do with your new best friend. Remember, you can enter \"0\" to check " 
+				+ petName + "'s status at any time."
+						+ "\n\n" + "Have Fun!");
+		System.out.println();
+		promptEnterKey();
 		return petName;
 	}
+	
+	public static void promptEnterKey() {
+		System.out.println("Press \"ENTER\" to continue...");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+	}
+	
 
 }
