3c40bdbe20e2d381923040f6fb866625ed3ad9f2
Caleb <sharpcaleb007@gmail.com>
2019-08-27 22:25:36 -0400

    updates to methods

--- src/main/java/virtualpet/VirtualPet.java
+++ src/main/java/virtualpet/VirtualPet.java
@@ -6,6 +6,7 @@ public class VirtualPet {
 	Integer thirst;
 	Integer tiredness;
 	Integer boredom;
+	Integer mailCount;
 	Boolean isBored;
 	Boolean needMail;
 	
@@ -24,94 +25,78 @@ public class VirtualPet {
 		thirst += 10;
 		tiredness += 10;
 		boredom += 15;
+		mailCount += 5;
 	}
 	
-	public void status() {
-		System.out.println("              " + name);
-		System.out.println("________________________________");
-		System.out.println();
-		System.out.println("Hunger level:		|  " + hunger);
-		System.out.println("Thirst level:		|  " + thirst);
-		System.out.println("Tiredness level:|  " + tiredness);
-		System.out.println("Boredom is:		|  " + boredom);
-		System.out.println("Mail needs checked:	|  " + needMail);
-	}
 	
 	public void firstSelection() {
-		System.out.println(name + " is all set! What would you like to do first?");
-		System.out.println();
-		System.out.println("Make a selection using numbers 0 through 6: ");
-		System.out.println("0. View Status");
-		System.out.println("1. Feed " + name);
-		System.out.println("2. Water " + name);
-		System.out.println("3. Play fetch with " + name);
-		System.out.println("4. Tell " + name + " to roll over");
-		System.out.println("5. Ask " + name + " to shake");
-		System.out.println("6. Have " + name + " check the mail");
-		System.out.println();
-		
+//		System.out.println(name + " is all set! What would you like to do first?");
+//		System.out.println();
+//		System.out.println("Make a selection using numbers 0 through 6: ");
+//		System.out.println("0. View Status");
+//		System.out.println("1. Feed " + name);
+//		System.out.println("2. Water " + name);
+//		System.out.println("3. Play fetch with " + name);
+//		System.out.println("4. Tell " + name + " to roll over");
+//		System.out.println("5. Ask " + name + " to shake");
+//		System.out.println("6. Have " + name + " check the mail");
+//		System.out.println();
+//		
 	}
-	public void selection() {
-		System.out.println("What would you like to do next?");
-		System.out.println();
-		System.out.println("Make a selection using numbers 0 through 6: ");
-		System.out.println("0. View Status");
-		System.out.println("1. Feed " + name);
-		System.out.println("2. Water " + name);
-		System.out.println("3. Play fetch with " + name);
-		System.out.println("4. Tell " + name + " to roll over");
-		System.out.println("5. Ask " + name + " to shake");
-		System.out.println("6. Have " + name + " check the mail");
-		System.out.println();
-		
-	}
+//	public void selection() {
+//		System.out.println("What would you like to do next?");
+//		System.out.println();
+//		System.out.println("Make a selection using numbers 0 through 6: ");
+//		System.out.println("0. View Status");
+//		System.out.println("1. Feed " + name);
+//		System.out.println("2. Water " + name);
+//		System.out.println("3. Play fetch with " + name);
+//		System.out.println("4. Tell " + name + " to roll over");
+//		System.out.println("5. Ask " + name + " to shake");
+//		System.out.println("6. Have " + name + " check the mail");
+//		System.out.println();
+//		
+//	}
 	
 	public void feed() {
-		System.out.println("You feed " + name + ". Mmmmmmmm");
-		System.out.println();
 		hunger -= 5;
 		thirst += 5;
 		tick();
 	}
 	
 	public void water() {
-		System.out.println("You give " + name + " water.");
-		System.out.println();
 		thirst -= 5;
 		hunger -= 5;
 		tick();
 	}
 	
 	public void fetch() {
-		System.out.println("You play fetch with " + name + ". ");
-		System.out.println();
 		boredom -= 20;
-		tiredness -= 10;
+		tiredness += 10;
 		hunger += 15;
 		thirst += 10;
 		tick();
 	}
 	
 	public void rollOver() {
-		System.out.println("You ask " + name + " to roll over.");
-		System.out.println();
 		boredom -= 10;
-		tiredness -= 5;
+		tiredness += 5;
 		hunger += 5;
 		thirst += 5;
 		tick();
 	}
 	
 	public void shake() {
-		System.out.println(name + " shakes your hand!");
-		System.out.println();
 		boredom -= 5;
 		tick();
 	}
 	
+	public void sleep() {
+		tiredness -= 20;
+		hunger += 5;
+	}
+	
 	public void checkMail() {
-		System.out.println(name + " checks the mail. Maybe there will be somehting fun in there!!");
-		System.out.println();
 	}
 	
 }
--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -10,53 +10,145 @@ public class VirtualPetApp {
 		String petName = introduction();
 		VirtualPet pet = new VirtualPet(petName);
 
-		pet.status();
+//		pet.status();
+		System.out.println("              " + pet.name);
+		System.out.println("________________________________");
 		System.out.println();
+		System.out.println("Hunger level:		|  " + pet.hunger);
+		System.out.println("Thirst level:		|  " + pet.thirst);
+		System.out.println("Tiredness level:	|  " + pet.tiredness);
+		System.out.println("Boredom is:		|  " + pet.boredom);
+		System.out.println("Mail needs checked:	|  " + pet.needMail);
+		System.out.println();
+
+//		System.out.println();
 		promptEnterKey();
-		pet.firstSelection();
+//		pet.firstSelection();
+		System.out.println(pet.name + " is all set! What would you like to do first?");
+		System.out.println();
+		System.out.println("Make a selection using numbers 0 through 7: ");
+		System.out.println("0. View Status");
+		System.out.println("1. Feed " + pet.name);
+		System.out.println("2. Water " + pet.name);
+		System.out.println("3. Play fetch with " + pet.name);
+		System.out.println("4. Tell " + pet.name + " to roll over");
+		System.out.println("5. Ask " + pet.name + " to shake");
+		System.out.println("6. Have " + pet.name + " check the mail");
+		System.out.println("7. Take a nap with " + pet.name);
+		System.out.println();
+		
 
 		int makeSelection = -1;
 
 		while (pet.hunger <= 200 && pet.thirst <= 200 && pet.tiredness <= 200 && pet.boredom <= 200) {
 			makeSelection = readInput();
-			
+//			if(pet.mailCount > 25) {
+//				pet.needMail = true;
+//				System.out.println("I think it's time " + pet.name + " checked the mail.");
+//			}
 				switch (makeSelection) {
 				case 0:
-					pet.status();
-					promptEnterKey();
+//					pet.status();
+					System.out.println("              " + pet.name);
+					System.out.println("________________________________");
+					System.out.println();
+					System.out.println("Hunger level:		|  " + pet.hunger);
+					System.out.println("Thirst level:		|  " + pet.thirst);
+					System.out.println("Tiredness level:	|  " + pet.tiredness);
+					System.out.println("Boredom is:		|  " + pet.boredom);
+					System.out.println("Mail needs checked:	|  " + pet.needMail);
+					System.out.println();
 					break;
 				case 1:
+//					if(pet.thirst > 100 && pet.hunger < 75) {
+//						System.out.println(pet.name + " is really thirsty. Consider food instead");
+//						break;
+//					}
+					System.out.println("You feed " + pet.name + ". Mmmmmmmm");
+					System.out.println();
 					pet.feed();
-					promptEnterKey();
 					break;
 				case 2:
+//					if(pet.hunger > 100 && pet.thirst < 75) {
+//						System.out.println("You should think about feeding " + pet.name + ".");
+//						break;
+//					}
+					System.out.println("You give " + pet.name + " water. Gulp, Gulp, Gulp.");
+					System.out.println();
 					pet.water();
-					promptEnterKey();
 					break;
 				case 3:
+//					if(pet.hunger > 100) {
+//						System.out.println(pet.name + " is to hungry to be chasing after things right now.");
+//						break;
+//					} else if(pet.thirst > 100) {
+//						System.out.println(pet.name + " is to thirsty to be running around.");
+//						break;
+//					} else if(pet.tiredness > 50) {
+//						System.out.println("Lets get " + pet.name + " some sleep. Zzzzzzzz");
+//						break;
+//					}
+					System.out.println("You play fetch with " + pet.name + ". Wooohoooo!");
+					System.out.println();
 					pet.fetch();
-					promptEnterKey();
 					break;
 				case 4:
+//					if(pet.tiredness > 50) {
+//						System.out.println("Lets get " + pet.name + " some sleep. Zzzzzzzz");
+//						break;
+//					}
+					System.out.println("You ask " + pet.name + " to roll over. Way to go!");
+					System.out.println();
 					pet.rollOver();
-					promptEnterKey();
 					break;
 				case 5:
+//					if(pet.tiredness > 50) {
+//						System.out.println("Lets get " + pet.name + " some sleep. Zzzzzzzz");
+//						break;
+//					}
+					System.out.println(pet.name + " shakes your hand! Very impressive ;)");
+					System.out.println();
 					pet.shake();
-					promptEnterKey();
 					break;
 				case 6:
+					System.out.println(pet.name + " checks the mail. Maybe there will be somehting fun in there!!");
+					System.out.println();
 					pet.checkMail();
-					promptEnterKey();
+					break;
+				case 7:
+					System.out.println("Nap time!");
+					System.out.println();
+					pet.sleep();
 					break;
 				}
-				pet.selection();
+				promptEnterKey();
+//				pet.selection();
+				System.out.println("What would you like to do next?");
+				System.out.println();
+				System.out.println("Make a selection using numbers 0 through 6: ");
+				System.out.println("0. View Status");
+				System.out.println("1. Feed " + pet.name);
+				System.out.println("2. Water " + pet.name);
+				System.out.println("3. Play fetch with " + pet.name);
+				System.out.println("4. Tell " + pet.name + " to roll over");
+				System.out.println("5. Ask " + pet.name + " to shake");
+				System.out.println("6. Have " + pet.name + " check the mail");
+				System.out.println();
 			}
 		if(pet.hunger > 200 || pet.thirst > 200 || pet.tiredness > 200 || pet.boredom > 200) {
 			System.out.println(pet.name + "didn't get the attention they needed. \n\n"
 					+ "Game Over.");
 		} 
-		pet.status();
+//		status();
+//		System.out.println("              " + pet.name);
+//		System.out.println("________________________________");
+//		System.out.println();
+//		System.out.println("Hunger level:		|  " + pet.hunger);
+//		System.out.println("Thirst level:		|  " + pet.thirst);
+//		System.out.println("Tiredness level:	|  " + pet.tiredness);
+//		System.out.println("Boredom is:		|  " + pet.boredom);
+//		System.out.println("Mail needs checked:	|  " + pet.needMail);
+//		System.out.println();
 		}
 
 	static String introduction() {
@@ -68,11 +160,11 @@ public class VirtualPetApp {
 		System.out.println();
 		System.out.println("This is so exciting! Let's get started.");
 		promptEnterKey();
-		System.out.print("Please enter the type of pet you want: ");
+		System.out.print("Please enter the type of pet you want and press enter: ");
 		String petType;
 		petType = input.nextLine();
 		System.out.println();
-		System.out.print("Great! Now enter your new " + petType + "'s name: ");
+		System.out.print("Great! Now enter your new " + petType + "'s name and press enter: ");
 //		promptEnterKey();
 //		System.out.print("Enter your " + petType + "'s name: ");
 		String petName;
@@ -97,6 +189,18 @@ public class VirtualPetApp {
 		scanner.nextLine();
 	}
 	
+	static void status(String name, Integer hunger, Integer thirst, Integer tiredness, Integer boredom, Boolean needMail) {
+		System.out.println("              " + name);
+		System.out.println("________________________________");
+		System.out.println();
+		System.out.println("Hunger level:		|  " + hunger);
+		System.out.println("Thirst level:		|  " + thirst);
+		System.out.println("Tiredness level:	|  " + tiredness);
+		System.out.println("Boredom is:		|  " + boredom);
+		System.out.println("Mail needs checked:	|  " + needMail);
+		System.out.println();
+		}
+
 	private static int readInput() {
 		int selection = -1;
 		if(!input.hasNextInt()) {
@@ -105,8 +209,8 @@ public class VirtualPetApp {
 			selection  = readInput();
 		} else if(input.hasNextInt()) {
 			selection = input.nextInt();
-			if(selection < 0 || selection > 6) {
-				System.out.println("Valid numbers are 0 through 6.");
+			if(selection < 0 || selection > 7) {
+				System.out.println("Valid numbers are 0 through 7.");
 				selection = readInput();
 			}
 		}
